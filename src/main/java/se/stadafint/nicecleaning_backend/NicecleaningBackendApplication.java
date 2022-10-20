2d37887655a5fc4355cffb4346ef3d46c2ee3378
Dyras <johan.forsberg@my.com>
2022-10-20 11:47:43 +0200

    Laddar upp diverse filer jag glömt

---
+++ src/main/java/se/stadafint/nicecleaning_backend/NicecleaningBackendApplication.java
@@ -0,0 +1,13 @@
+package se.stadafint.nicecleaning_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicecleaningBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NicecleaningBackendApplication.class, args);
+    }
+
+}
