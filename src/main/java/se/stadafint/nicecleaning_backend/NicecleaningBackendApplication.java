ab74cc4383dba24a9b2c369b21ac988795cd4a8e
Dyras <johan.forsberg@my.com>
2022-10-20 13:39:36 +0200

    Lägger till AutoWire + en metod som försvann

--- src/main/java/se/stadafint/nicecleaning_backend/NicecleaningBackendApplication.java
+++ src/main/java/se/stadafint/nicecleaning_backend/NicecleaningBackendApplication.java
@@ -1,10 +1,19 @@
 package se.stadafint.nicecleaning_backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import se.stadafint.nicecleaning_backend.repo.CleanRepo;
+import se.stadafint.nicecleaning_backend.repo.UserRepo;
 
 @SpringBootApplication
 public class NicecleaningBackendApplication {
+
+    @Autowired
+    CleanRepo cleanRepo;
+
+    @Autowired
+    UserRepo userRepo;
 
     public static void main(String[] args) {
         SpringApplication.run(NicecleaningBackendApplication.class, args);
